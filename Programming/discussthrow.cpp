1ce3188f5d8c1bf30bc54a213718ad1fd50003f0
ShauryaPanwar <panwarshaurya11@gmail.com>
2022-08-25 20:02:32 +0530

    day1

---
+++ Programming/discussthrow.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a;
+    cin>>a;
+  
+    while(a--)
+    {
+        
+        int x,y,z;
+        cin>>x>>y>>z;
+        if(x>=y && x>=z)
+        {
+            cout<<x<<endl;
+        }
+        else if(y>=x && y>=z)
+        {
+            cout<<y<<endl;
+        } 
+        else
+        {
+            cout<<z<<endl;
+        }
+
+    }
+    return 0;
+
+}
